74c5940e4816dc767050e28d048a281eb19408a1
Bryan Grill <grillb@gmail.com>
2018-12-18 21:53:07 -0500

    add donation list

--- src/DonationList.js
+++ src/DonationList.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+import numeral from 'numeral';
+import { Segment, List, Header } from 'semantic-ui-react';
 
 const DonationList = ({ donations }) => {
   console.log(donations);
-  if (donations) {
-    return <div>List</div>;
+  if (!_.isEmpty(donations)) {
+    return (
+      <Segment basic>
+        <Header>Donations So Far</Header>
+        <List divided relaxed>
+        {_.map(donations, d => {
+          return (
+            <List.Item key={d.key}>
+              <Header sub>{d.name}</Header>
+              <span>{numeral(d.amount).format('$0,0.00')}</span>
+            </List.Item>
+          );
+        })}
+        </List>
+      </Segment>
+    );
   }
   return null;
 };
